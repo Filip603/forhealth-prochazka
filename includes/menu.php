74af567c448fcdfe2642bd3484a88e046e5ad98b
Filip Procházka <prochazkaf@ekodom.cz>
2025-11-07 12:55:00 +0100

    dodelat doma

---
+++ includes/menu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praxe</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="menu">
+
+        <div class="menu__logo">
+            <img class="logo" src="images/logo.png" alt="">
+        </div>
+
+        <div class="menu__part1">
+
+            <h2>
+              <a href=""> Produkty </a>
+            </h2>
+
+            <h2>
+               <a href=""> O nás </a>
+            </h2>
+
+            <h2>
+               <a href=""> Kontakty </a>
+            </h2>
+
+        </div>
+
+        <div class="menu__part2">
+
+           <a href=""> <img class="picture" src="images/uzivatel.png" alt=""> </a>
+           <a href=""><img class="picture"  src="images/kosik.png" alt=""> </a>
+
+        </div>
+
+    </div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+    include "includes/menu.php";
+?>
+
+
+
+    <div class="footer">
+
+        <div>
+            <h2>Kontakty</h2>
+            <h2>email:lllllllll</h2>
+        </div>
+
+        <div>
+            <img src="images/logo.png" alt="">
+        </div>
+
+        <div>
+            <img class="instalogo" src="" alt="">
+            <h2>nazev insta</h2>
+        </div>
+
+    </div>
+
+</body>
+</html>
